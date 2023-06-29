c1d2484685c59506000a0e36b98191ea046654f5
Soelie Lerch <soelie.lerch@lis-lab.fr>
2023-06-29 15:08:42 +0200

    modify class of database

--- class_metiers/class_transcript.py
+++ class_metiers/class_transcript.py
@@ -1,5 +1,6 @@
 class transcript:
-  def __init__(self, num_dialogue,text, emotion):
+  def __init__(self, id_video, num_dialogue,text, emotion):
+  	self.id_video=id_video
   	self.num_dialogue=num_dialogue
     self.text = text
     self.emotion = emotion
