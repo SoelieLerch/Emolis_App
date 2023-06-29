8284445cdb2fe257bc54a120d7bf084c27183246
Soelie Lerch <soelie.lerch@lis-lab.fr>
2023-06-29 15:01:04 +0200

    modify class of database

---
+++ class_metiers/class_transcript.py
@@ -0,0 +1,5 @@
+class transcript:
+  def __init__(self, num_dialogue,text, emotion):
+  	self.num_dialogue=num_dialogue
+    self.text = text
+    self.emotion = emotion
---
+++ class_metiers/class_video.py
@@ -0,0 +1,5 @@
+class Video:
+  def __init__(self, title, dialogue, rank_videos_reco):
+  	self.title=title
+  	self.dialogues=dialogues
+  	self.rank_videos_reco=rank_videos_reco
