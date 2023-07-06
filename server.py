98df279c4c174797b00338b19aaf6f44571cfd17
Soelie Lerch <soelie.lerch@lis-lab.fr>
2023-07-06 14:51:06 +0200

    add a new video and get all videos

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from class_metiers  import class_user
 
-from class_DAO import class_user_DAO
+from class_DAO import class_user_DAO, class_video_DAO
 from pydantic import BaseModel
 import uvicorn
+from fastapi.responses import JSONResponse
 
 
 class User(BaseModel):
@@ -11,19 +12,37 @@ class User(BaseModel):
 	age: int
 	genre : str
 
+class Video(BaseModel):
+	title: str
+	path: str
 
 
 app = FastAPI()
 
 @app.put("/user/")
-def create_user(user :  User, status_code=200):
+def create_user(user :  User, status_code=201):
 	userDAO=class_user_DAO.User_DAO()
 	user_exist=userDAO.find_user_from_name(user.login)
 	if user_exist==None :
 		userDAO.add_user(user.login, user.age, user.genre)
-		return {"login": user.login, "age": user.age, "genre":user.genre}
+		return JSONResponse(status_code=status.HTTP_201_CREATED, content={"login": user.login, "age": user.age, "genre":user.genre})
 	else :
 		raise HTTPException(status_code=405, detail="Error 405 : Login already exist")
+
+
+@app.put("/video/")
+def create_video(video :  Video, status_code=201):
+	videoDAO=class_video_DAO.Video_DAO()
+	video_exist=videoDAO.find_video_from_title(video.title)
+	path_exist=videoDAO.find_video_from_path(video.path)
+	if path_exist!=None :
+		return JSONResponse(status_code=status.HTTP_201_CREATED, content={"title": video.title, "path": video.path})
+	if video_exist==None :
+		videoDAO.add_Video(video.title, video.path)
+		return JSONResponse(status_code=status.HTTP_201_CREATED, content={"title": video.title, "path": video.path})
+	else :
+		raise HTTPException(status_code=405, detail="Error 405 : Title already exist")
+
 
 @app.get("/user/{login}")
 def get_user(login :str):
@@ -34,7 +53,14 @@ def get_user(login :str):
 	else :
 		raise HTTPException(status_code=404, detail="Error 404 : Login not found")
 
-
+@app.get("/video/")
+def find_all_videos(number, page):
+	videoDAO=class_video_DAO.Video_DAO()
+	videos=videoDAO.find_all(int(number), int(page))
+	data_request=[]
+	for v in videos :
+		data_request.append({"id_video":v.id_video, "Title":v.title, "Path":v.path})
+	return data_request
 
 
 if __name__ == "__main__":
--- test_api.py
+++ test_api.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from server import app
 from class_metiers import class_user
+from class_DAO import class_video_DAO
 
 client=TestClient(app)
 
@@ -9,7 +10,7 @@ def create_user(login, age, genre):
         "/user/",
         json={"login": login, "age": age, "genre":genre},
     )
-    assert response.status_code == 200, response.text
+    assert response.status_code == 201, response.text
     data = response.json()
     print(data)
 def get_user(login):
@@ -18,5 +19,28 @@ def get_user(login):
     assert response.status_code == 200, response.text
     data = response.json()
     print(data)
-    data=class_user.User(data["id_user"], data["login"], data["age"], data["genre"])
     return data
+
+def create_video(title, path):
+    response = client.put(
+        "/video/",
+        json={"title": title, "path":  path},
+    )
+    print(response.status_code)
+    assert response.status_code == 201, response.text
+    data = response.json()
+    print(data)
+
+def find_all_videos(number, page):
+    response = client.get(
+        "/video?number="+str(number)+"&page="+str(page))
+    assert response.status_code == 200, response.text
+    data = response.json()
+    print(data)
+    return data
+
+
+#create_user("lilou783", 28, "femme")
+user=get_user("lilou783")
+create_video("baby2", "path")
+find_all_videos(10,0)
